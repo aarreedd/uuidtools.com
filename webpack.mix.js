0c77f7d70484762d3d5da39ce7d2fe3ce6ed4837
Alan Reed <alan@reedtechsol.com>
2019-12-29 21:02:13 -0500

    version all the things

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .copy('node_modules/fitty/dist/fitty.min.js', 'public/js/fitty.js')
-   .copy('node_modules/browser-filesaver/FileSaver.min.js', 'public/js/FileSaver.js');
+   .copy('node_modules/browser-filesaver/FileSaver.min.js', 'public/js/FileSaver.js')
+   .version();
